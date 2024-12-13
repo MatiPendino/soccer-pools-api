85b9f1c795cd1e84bc92fd0f601713af3f8126b9
Matías <matiaspendino76@gmail.com>
2024-12-13 21:03:59 +1100

    perf: MatchResultsUpdateApiView improved

--- SoccerPools/apps/match/serializers.py
+++ SoccerPools/apps/match/serializers.py
@@ -4,14 +4,15 @@ from .models import MatchResult
 class MatchResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = MatchResult
-        fields = ('goals_team_1', 'goals_team_2', 'match')
+        fields = ('id', 'goals_team_1', 'goals_team_2', 'match')
 
     def to_representation(self, instance):
         return {
+            'id': instance.id,
             'goals_team_1': instance.goals_team_1,
             'goals_team_2': instance.goals_team_2,
             'team_1': instance.match.get_team_acronyms()[0],
             'team_2': instance.match.get_team_acronyms()[1],
-            'badge_team_1': instance.match.get_team_badges()[0],
-            'badge_team_2': instance.match.get_team_badges()[1]
+            'badge_team_1': instance.match.get_team_badges()[0] if instance.match.team_1.badge else '',
+            'badge_team_2': instance.match.get_team_badges()[1] if instance.match.team_2.badge else ''
         }
--- SoccerPools/apps/match/views.py
+++ SoccerPools/apps/match/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, permissions, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, get_list_or_404
 from .serializers import MatchResultSerializer
 from .models import MatchResult
 
@@ -15,7 +16,7 @@ class MatchResultsListCreateApiView(generics.ListCreateAPIView):
         return match_results
     
     def post(self, request):
-        serializer = MatchResultSerializer(request.data, many=True)
+        serializer = MatchResultSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -27,6 +28,21 @@ class MatchResultsUpdateApiView(APIView):
     permission_class = (permissions.IsAuthenticated,)
 
     def post(self, request):
-        print(request.data)
+        match_results_data = request.data.get('matchResults')
 
-        return Response({'tuki': 'ok'})
+        match_results_ids = [match_result['id'] for match_result in match_results_data]
+        match_results = get_list_or_404(MatchResult, id__in=match_results_ids)
+        
+        # Create a mapping of match result objects by ID
+        match_result_map = {match_result.id: match_result for match_result in match_results}
+
+        # Update the fields in bulk
+        for result_data in match_results_data:
+            match_result = match_result_map.get(result_data['id'])
+            if match_result:
+                match_result.goals_team_1 = result_data['goals_team_1']
+                match_result.goals_team_2 = result_data['goals_team_2']
+
+        MatchResult.objects.bulk_update(match_results, ['goals_team_1', 'goals_team_2'])
+
+        return Response({'success': 'Match results updated successfully!'})
