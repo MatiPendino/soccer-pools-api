007e0949ad7a6af9256616cebeeb6bf5ce8d9ab9
Mati <matiaspendino76@gmail.com>
2025-01-29 12:07:17 +1100

    feat: django-debug-toolbar settings

--- SoccerPools/SoccerPools/settings/local.py
+++ SoccerPools/SoccerPools/settings/local.py
@@ -4,6 +4,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+'''DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: False,
+}'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
