c201eb228f29d5e2b721b719602e4470e7550542
Mati <matiaspendino76@gmail.com>
2025-03-03 09:32:02 +1100

    style: dead code removed

--- SoccerPools/SoccerPools/settings/local.py
+++ SoccerPools/SoccerPools/settings/local.py
@@ -8,10 +8,6 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-'''DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda request: False,
-}'''
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
