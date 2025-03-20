d347c30f82edd5ce3fe0cb3be60b681e38f8d41b
Mati <matiaspendino76@gmail.com>
2025-03-20 11:05:34 +1100

    fix (FCMToken): user ForeignKey typo fixed

--- SoccerPools/apps/notification/models.py
+++ SoccerPools/apps/notification/models.py
@@ -5,7 +5,7 @@ from apps.league.models import League
 
 class FCMToken(BaseModel):
     token_id = models.CharField(max_length=255)
-    user = models.ForeignKey(AppUser, on_delete=models.Case, related_name='fcm_tokens')
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='fcm_tokens')
     device_id = models.CharField(max_length=255, null=True, blank=True)
     leagues = models.ManyToManyField(League)
 
